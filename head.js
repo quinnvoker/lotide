c33b7a05794f2ce0c3214824979ada7aedb4c16c
Quinn <quinnvoker@gmail.com>
2020-07-20 23:43:22 +0000

    added head function and tests

--- head.js
+++ head.js
@@ -5,3 +5,14 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[0];
+}
+
+assertEqual(2, head([2,3,4]));
+assertEqual("house", head(["house","apartment","condo"]));
+assertEqual(undefined, head([]));
