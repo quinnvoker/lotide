ae082dfdc5c691512fd36611ccfdc310cd6ddd5c
Quinn <quinnvoker@gmail.com>
2020-07-21 23:14:06 +0000

    implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/*
+  function eqArrays(arrayA, arrayB):
+    if arrays length are not equal
+      return false
+    for each element of arrayA (with iterator i):
+      check if arrayA[i] !== arrayB[i]
+        if so, return false
+    return true
+*/
+
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
