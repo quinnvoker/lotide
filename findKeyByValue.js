60eca77c9f86d3a53eb351269c4094b79fc20043
Quinn <quinnvoker@gmail.com>
2020-07-22 22:12:41 +0000

    implemented findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,10 +7,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = (object, value) => {
-
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
