f854ee05c1689c9c9bad75b56da86021be8da254
Quinn <quinnvoker@gmail.com>
2020-07-22 20:03:27 +0000

    added skeleton of middle

---
+++ middle.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+};
