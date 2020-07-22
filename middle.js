980ab5950b4836cc2430954d3f154f1b594e9af0
Quinn <quinnvoker@gmail.com>
2020-07-22 20:15:18 +0000

    added function skeleton and test cases for middle

--- middle.js
+++ middle.js
@@ -29,3 +29,14 @@ const assertArraysEqual = function(arrayA, arrayB) {
       else:
         return [ array[Math.floor[middleIndex]]]
 */
+
+const middle = (array) => {
+
+};
+
+const testEven = [1, 2, 3, 4];
+const testOdd = [1, 2, 3, 4, 5];
+const testShort = ['hello', 'world'];
+assertArraysEqual(middle(testEven), [2, 3]);
+assertArraysEqual(middle(testOdd), [3]);
+assertArraysEqual(middle(testShort), []);
