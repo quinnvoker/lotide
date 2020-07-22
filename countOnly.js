479a4bf866dc5b4989f12c7e5068108da2213709
Quinn <quinnvoker@gmail.com>
2020-07-22 20:49:47 +0000

    added test cases to countOnly

--- countOnly.js
+++ countOnly.js
@@ -6,7 +6,27 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countOnly = () => {
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = (allItems, itemsToCount) => {
 
 }
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
 
