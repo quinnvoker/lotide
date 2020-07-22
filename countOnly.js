bab75f0247b7a6ddc7de4b21c60db0eafe923bdf
Quinn <quinnvoker@gmail.com>
2020-07-22 20:57:17 +0000

    implemented countOnly

--- countOnly.js
+++ countOnly.js
@@ -9,8 +9,18 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = (allItems, itemsToCount) => {
-
-}
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }    
+  }
+  return results;
+};
 
 const firstNames = [
   "Karl",
