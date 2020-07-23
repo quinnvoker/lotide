1cfbeccece6db0025992ccff37d2c6d2ba76aecc
Quinn <quinnvoker@gmail.com>
2020-07-23 22:26:08 +0000

    added findKey skeleton and tests

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+
+};
+
+const player = {
+  hp: 31,
+  mp: 50,
+  exp: 4048,
+  level: 10,
+};
+
+const meals = {
+  breakfast: 'cereal',
+  lunch: 'hotdog',
+  dinner: 'steak',
+};
+
+const scores = {
+  jim: 75,
+  mary: 84,
+  angela: 95,
+  stewart: 99,
+};
+
+assertEqual(findKey(player, stat => stat % 10 === 0), 'mp');
+assertEqual(findKey(meals, meal => meal === 'steak'), 'dinner');
+assertEqual(findKey(scores, score => score > 90), 'angela');
