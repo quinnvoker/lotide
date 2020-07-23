19147a08b769010074b1e22c7d0707111c4ec27e
Quinn <quinnvoker@gmail.com>
2020-07-23 21:56:53 +0000

    implemented map and tests

---
+++ map.js
@@ -0,0 +1,48 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//tests
+const people = [
+  {
+    name: 'Jason',
+    age: 3,
+  },
+  {
+    name: 'Tom',
+    age: 16,
+  },
+  {
+    name: "Frieda",
+    age: 24,
+  }
+]
+assertArraysEqual(map(people, person => person.name), ['Jason', 'Tom', 'Frieda']);
+assertArraysEqual(map([1, 2, 3], num => num * 5), [5, 10, 15]);
+assertArraysEqual(map(['hello', 'world'], word => word.slice(0, 2)), ['he', 'wo']);
