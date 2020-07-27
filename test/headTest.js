d98158311e42768d80a92103c197694122dd0d0e
Quinn <quinnvoker@gmail.com>
2020-07-27 20:43:51 +0000

    migrated head test to mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,16 @@
+const expect = require('chai').expect;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(2, head([2,3,4]));
-assertEqual("house", head(["house","apartment","condo"]));
-assertEqual(undefined, head([]));
+describe('#head', () => {
+  it('should return 2 for [2]', ()  => {
+    expect(head([2])).to.equal(2);
+  });
+
+  it('should return "house" for ["house","apartment","condo"]', () => {
+    expect(head(["house","apartment","condo"])).to.equal('house');
+  });
+
+  it('should return undefined if given an empty array', () => {
+    expect(head([])).to.be.undefined;
+  });
+});
