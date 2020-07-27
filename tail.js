9c4c3147043557b2f77bcd1af6dcfc58951c4418
Quinn <quinnvoker@gmail.com>
2020-07-27 20:11:55 +0000

    refactored tail

--- tail.js
+++ tail.js
@@ -1,20 +1,8 @@
-const assertEqual = require('./assertEqual');
-
 const tail = function(array) {
   if (array.length < 2) {
     return [];
   }
   return array.slice(1);
 };
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail(words);
-console.log(result);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
 module.exports = tail;
