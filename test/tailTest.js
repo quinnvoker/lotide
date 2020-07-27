b7d0a46023e451c298ded7b647548d618e2c5849
Quinn <quinnvoker@gmail.com>
2020-07-27 20:55:01 +0000

    migrated tail tests to mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,20 @@
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual');
+const expect = require('chai').expect;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe('#tail', () => {
 
-const result = tail(words);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+  it('should return an array without the first element', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    expect(tail(words)).to.deep.equal(["Lighthouse", "Labs"]);
+  });
+
+  it('should not modify the length of the array it is given', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    expect(words.length).to.equal(3);
+  });
+
+  it('should return an empty array if given an array with less than 2 elements', () => {
+    expect(tail([1])).to.deep.equal([]);
+  });
+});
