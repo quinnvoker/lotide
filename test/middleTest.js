c0e292fdc04935a47a04d13f0bc5507e6a227dee
Quinn <quinnvoker@gmail.com>
2020-07-27 21:02:14 +0000

    migrated middle tests to mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,19 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const expect = require('chai').expect;
 
-const testEven = [1, 2, 3, 4];
-const testOdd = [1, 2, 3, 4, 5];
-const testShort = ['hello', 'world'];
-assertArraysEqual(middle(testEven), [2, 3]);
-assertArraysEqual(middle(testOdd), [3]);
-assertArraysEqual(middle(testShort), []);
+describe('#middle', () => {
+  it('should return an array containing the middle element from an odd length array', () => {
+    const testOdd = [1, 2, 3, 4, 5];
+    expect(middle(testOdd)).to.deep.equal([3]);
+  });
+
+  it('should return an array containing the middle two elements from an even length array', () => {
+    const testEven = [1, 2, 3, 4];
+    expect(middle(testEven)).to.deep.equal([2, 3]);
+  });
+
+  it('should return an empty array when given an array with length < 3', () => {
+    const testShort = ['hello', 'world'];
+    expect(middle(testShort)).to.deep.equal([]);
+  });
+});
