8cd24bf63d4c8cf1be1d3408eb75214c167b672c
Quinn <quinnvoker@gmail.com>
2020-07-27 20:14:17 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-const assertEqual = require('./assertEqual');
-/*
-  function eqArrays(arrayA, arrayB):
-    if arrays length are not equal
-      return false
-    for each element of arrayA (with iterator i):
-      check if arrayA[i] !== arrayB[i]
-        if so, return false
-    return true
-*/
-
 const eqArrays = function(arrayA, arrayB) {
   if (arrayA.length !== arrayB.length) {
     return false;
@@ -23,14 +12,5 @@ const eqArrays = function(arrayA, arrayB) {
   }
   return true;
 };
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-// recursive tests
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
 
 module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+// recursive tests
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
